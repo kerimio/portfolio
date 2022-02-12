b7b4be623c2db214c83ec48dee643ddc6b993bc1
kerimio <bjkerim@web.de>
2022-02-12 18:45:06 +0100

    console bug fixed

--- portfolio/src/components/Console/Console.js
+++ portfolio/src/components/Console/Console.js
@@ -1,20 +1,14 @@
 import "./Console.css";
 const Console = () => {
-  return (
-    <div class="console">
-      <header>
-        <p>guest@kerim</p>
-      </header>
-      <div class="consolebody">
-        <div className="typeWriter">
-          <p>&gt; C:\Users\Arbeit> npm i herzlich-willkommen</p>
-        </div>
-        <div className="command">
-          <p>&gt; test ya salame</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Console;
+    return (
+        <div className="console">
+  <header>
+    <p>guest@kerim</p>
+  </header>
+  <div className="consolebody">
+    <p>&gt; C:\Users\Arbeit>  npm i herzlich-willkommen</p>
+  </div>
+  
+  </div>
+    )
+}
